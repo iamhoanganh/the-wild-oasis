73cf9b98e8af18af17969dd8d7e4094325c39633
Hoàng Anh <iamnguyenhoanganh@outlook.com>
2023-08-09 16:40:15 +0700

    styled components

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,24 +1,24 @@
 import styled, { css } from "styled-components";
 
-// const sizes = {
-//   small: css`
-//     font-size: 1.2rem;
-//     padding: 0.4rem 0.8rem;
-//     text-transform: uppercase;
-//     font-weight: 600;
-//     text-align: center;
-//   `,
-//   medium: css`
-//     font-size: 1.4rem;
-//     padding: 1.2rem 1.6rem;
-//     font-weight: 500;
-//   `,
-//   large: css`
-//     font-size: 1.6rem;
-//     padding: 1.2rem 2.4rem;
-//     font-weight: 500;
-//   `,
-// };
+const sizes = {
+  small: css`
+    font-size: 1.2rem;
+    padding: 0.4rem 0.8rem;
+    text-transform: uppercase;
+    font-weight: 600;
+    text-align: center;
+  `,
+  medium: css`
+    font-size: 1.4rem;
+    padding: 1.2rem 1.6rem;
+    font-weight: 500;
+  `,
+  large: css`
+    font-size: 1.6rem;
+    padding: 1.2rem 2.4rem;
+    font-weight: 500;
+  `,
+};
 
 const variations = {
   primary: css`
@@ -48,14 +48,14 @@ const variations = {
   `,
 };
 const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem;
-  font-weight: 500;
   border: none;
   border-radius: var(--border-radius-sm);
-  background-color: var(--color-brand-600);
-  color: var(--color-brand-50);
-  cursor: pointer;
   box-shadow: var(--shadow-sm);
+  ${(props) => sizes[props.size]}
+  ${(props) => variations[props.variation]}
 `;
+Button.defaultProps = {
+  variation: "primary",
+  size: "medium",
+};
 export default Button;
