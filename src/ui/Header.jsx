b17b66ab6c55b9eb1318ae5773c40520c11a4f6c
iamhoanganh <anhnh@teracom.vn>
2024-02-28 22:05:36 +0700

    [fea] building app header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,20 @@
 import { styled } from "styled-components";
-import Logout from 'src/features/authentication/Logout.jsx'
+import HeaderMenu from 'src/ui/HeaderMenu.jsx'
+import UserAvatar from 'src/features/authentication/UserAvatar.jsx'
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 2.4rem;
 `;
 function Header() {
   return <StyledHeader>
-    <Logout />
+    <UserAvatar />
+    <HeaderMenu />
   </StyledHeader>;
 }
 
---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,27 @@
+import ButtonIcon from 'src/ui/ButtonIcon.jsx'
+import { HiOutlineUser } from 'react-icons/hi2'
+import Logout from 'src/features/authentication/Logout.jsx'
+import { useNavigate } from 'react-router-dom'
+import styled from 'styled-components'
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  gap: 0.4rem`
+
+function HeaderMenu () {
+  const navigate = useNavigate()
+  return (
+    <StyledHeaderMenu>
+      <li>
+        <ButtonIcon onClick={() => navigate('/account')}>
+          <HiOutlineUser/>
+        </ButtonIcon>
+      </li>
+      <li>
+        <Logout/>
+      </li>
+    </StyledHeaderMenu>
+  )
+}
+
+export default HeaderMenu
