01673a44feffd13cbe453712308eb2505b4646f3
iamhoanganh <anhnh@teracom.vn>
2024-02-29 21:35:27 +0700

    [fea] computing recent booking and stay

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,5 +1,8 @@
 // import React from 'react'
 import styled from "styled-components";
+import { useRecentBooking } from 'src/features/dashboard/useRecentBooking.js'
+import Spinner from 'src/ui/Spinner.jsx'
+import { useRecentStay } from 'src/features/dashboard/useRecentStay.js'
 // import PropTypes from 'prop-types'
 
 const StyledDashboardLayout = styled.div`
@@ -10,6 +13,12 @@ const StyledDashboardLayout = styled.div`
 `;
 
 const DashboardLayout = () => {
+  const {isLoading : isLoadingBooking, bookings} = useRecentBooking()
+  const {isLoading : isLoadingStay, stays, confirmedStays} = useRecentStay()
+  if (isLoadingBooking || isLoadingStay) {
+    return <Spinner />
+  }
+  console.log("bookings", bookings, stays, confirmedStays)
   return (
     <StyledDashboardLayout>
         <div>Static</div>
